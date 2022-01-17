72ac06ff7f59cb1aabbc8c8b00190a7e30e1f7b6
mkjanke <mkjanke@gmail.com>
2022-01-17 17:16:37 -0600

    Create DS Object

--- include/ds18b20.h
+++ include/ds18b20.h
@@ -8,112 +8,145 @@
 #define PARASITE_POWER  false
 static Placeholder<OneWireNg_CurrentPlatform> _ow;
 
-class tempSensor {
+// Class to encapsulate a DS18B20 sensor pair. 
+// Temperature A is will always be the higher temperature sensor, 
+// so that the fans will spin in an attempt to lower that temp.
+// Temperature B is the base (ambient) temperature sensor, presumed to be lower temperature
+// 
+// Will find the first two DS sensor on a single OneWire bus.
+
+class sensorPair {
   
+  private:
+    uint8_t _pin;
+
   public:
-    tempSensor(uint8_t pin, uint8_t type )
-    {
+    sensorPair(uint8_t pin, uint8_t pinB, uint8_t type ) {
+      // 'pinB' is a dummy paraameter, to be consistent with DHT11 version
       _pin = pin;
     };
     
-    float temperature = 0.0;
+    float temperatureA = 0.0;
+    float temperatureB = 0.0;
+
     int readCount = 0; 
     int errorCount = 0;
-    uint8_t _pin;
 
-    void begin()
-    {      
+    bool isCelsius;
+    bool isFahrenheit;
+
+    void begin() {      
       new (&_ow) OneWireNg_CurrentPlatform(_pin, false);
       DSTherm drv(_ow);
       drv.filterSupportedSlaves();
-
     }
 
-    float readFahrenheit(){
-        DSTherm drv(_ow);
-        Placeholder<DSTherm::Scratchpad> _scrpd;
-        long tempF;
-        
-        /* convert temperature on all sensors connected... */
-        drv.convertTempAll(DSTherm::SCAN_BUS, PARASITE_POWER);
+    bool readFahrenheit(){
+      isFahrenheit = true;
+      isCelsius = false;
+      return readTemperature(TEMP_FAHRENHEIT);
+    }
+    bool readCelsius(){
+      isFahrenheit = false;
+      isCelsius = true;
+      return readTemperature(false);
+    }
 
-        /* ...and read them one-by-one */
-        for (const auto& id: (OneWireNg&)_ow) {
-            if (printId(id)) {
-                if (drv.readScratchpad(id, &_scrpd) == OneWireNg::EC_SUCCESS)
-                {
-                    printScratchpad(_scrpd);
-                    tempF = _scrpd.operator&()->getTemp();
-                }
-                else
-                    Serial.println("  Invalid CRC!");
+  private:
+    bool readTemperature(bool fahrenheit) {
+      DSTherm drv(_ow);
+      Placeholder<DSTherm::Scratchpad> _scrpd;
+
+      long tempT[2];
+      uint8_t i = 0;
+      
+      /* convert temperature on all sensors connected... */
+      drv.convertTempAll(DSTherm::SCAN_BUS, PARASITE_POWER);
+
+      /* ...and read them one-by-one */
+      for (const auto& id: (OneWireNg&)_ow) {
+        if (printId(id)) {
+            if (drv.readScratchpad(id, &_scrpd) == OneWireNg::EC_SUCCESS) {
+                printScratchpad(_scrpd);
+                tempT[i] = _scrpd.operator&()->getTemp();
+                i++;
+            }
+            else {
+              Serial.println("  Invalid CRC!");
+              errorCount++;
+              return false;
             }
         }
-        Serial.println("----------");
-
-      // float tempF = 42;
-      if (!isnan(tempF)) {
-        temperature =  (float)((tempF + 50)/100)/10 ;
-//        Serial.println(temperature);
-        readCount++;
-        return true;
-      } else {
-        errorCount++;
-        return false;
       }
+      Serial.println("----------");
+
+      if (!isnan(tempT[0]) && !isnan(tempT[1])) {
+        readCount++;
+        if (tempT[0] > tempT[1] ) {
+          temperatureA = (float)((tempT[0] + 50)/100)/10;
+          temperatureB = (float)((tempT[1] + 50)/100)/10;
+        } else {
+          temperatureA = (float)((tempT[1] + 50)/100)/10;
+          temperatureB = (float)((tempT[0] + 50)/100)/10;
+        }
+        if (fahrenheit){
+          temperatureA = (9.0*temperatureA/5.0)+32;
+          temperatureB = (9.0*temperatureB/5.0)+32;
+        }
+        return true;
+      } 
+      else 
+        return false;
     }
 
+    static bool printId(const OneWireNg::Id& id) {
+      const char *name = DSTherm::getFamilyName(id);
 
-  static bool printId(const OneWireNg::Id& id)
-  {
-    const char *name = DSTherm::getFamilyName(id);
+      Serial.print(id[0], HEX);
+      for (size_t i = 1; i < sizeof(OneWireNg::Id); i++) {
+          Serial.print(':');
+          Serial.print(id[i], HEX);
+      }
+      if (name) {
+          Serial.print(" -> ");
+          Serial.print(name);
+      }
+      Serial.println();
 
-    Serial.print(id[0], HEX);
-    for (size_t i = 1; i < sizeof(OneWireNg::Id); i++) {
-        Serial.print(':');
-        Serial.print(id[i], HEX);
-    }
-    if (name) {
-        Serial.print(" -> ");
-        Serial.print(name);
-    }
-    Serial.println();
-
-    return (name != NULL);
-  }
-
-  static void printScratchpad(const DSTherm::Scratchpad& scrpd)
-  {
-    const uint8_t *scrpd_raw = scrpd.getRaw();
-
-    Serial.print("  Scratchpad:");
-    for (size_t i = 0; i < DSTherm::Scratchpad::LENGTH; i++) {
-        Serial.print(!i ? ' ' : ':');
-        Serial.print(scrpd_raw[i], HEX);
+      return (name != NULL);
     }
 
-    Serial.print("; Th:");
-    Serial.print(scrpd.getTh());
+    static void printScratchpad(const DSTherm::Scratchpad& scrpd) {
+      const uint8_t *scrpd_raw = scrpd.getRaw();
 
-    Serial.print("; Tl:");
-    Serial.print(scrpd.getTl());
+      Serial.print("  Scratchpad:");
+      for (size_t i = 0; i < DSTherm::Scratchpad::LENGTH; i++) {
+          Serial.print(!i ? ' ' : ':');
+          Serial.print(scrpd_raw[i], HEX);
+      }
 
-    Serial.print("; Resolution:");
-    Serial.print(9 + (int)(scrpd.getResolution() - DSTherm::RES_9_BIT));
+      Serial.print("; Th:");
+      Serial.print(scrpd.getTh());
 
-    long temp = scrpd.getTemp();
-    Serial.print("; Temp:");
-    if (temp < 0) {
-        temp = -temp;
-        Serial.print('-');
+      Serial.print("; Tl:");
+      Serial.print(scrpd.getTl());
+
+      Serial.print("; Resolution:");
+      Serial.print(9 + (int)(scrpd.getResolution() - DSTherm::RES_9_BIT));
+
+      long temp = scrpd.getTemp();
+      Serial.print("; Temp:");
+      if (temp < 0) {
+          temp = -temp;
+          Serial.print('-');
+      }
+      Serial.print(temp / 1000);
+      Serial.print('.');
+      Serial.print(temp % 1000);
+      Serial.print(" C");
+
+      Serial.println();
     }
-    Serial.print(temp / 1000);
-    Serial.print('.');
-    Serial.print(temp % 1000);
-    Serial.print(" C");
-
-    Serial.println();
-  }
 
 };
 
